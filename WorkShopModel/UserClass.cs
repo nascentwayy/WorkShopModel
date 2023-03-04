800e73531c77da5c0ff3aab0fa105a5cd5731309
Шевцов Дмитрий <nascentwayyy@yandex.ru>
2023-03-04 18:08:12 +0300

    Версия, которая работает некорректно

--- WorkShopModel/UserClass.cs
+++ WorkShopModel/UserClass.cs
@@ -8,4 +8,16 @@
         Orders = new List<Basket>();
         Name = name;
     }
+
+    public void GetListOrderts()
+    {
+        for (int i = 0; i < Orders.Count; i++)
+        {
+            Console.WriteLine($"Заказ №{i + 1}");
+            for (int z = 0; z < Orders[i].BasketProd.Count; z++)
+            {
+                Console.WriteLine(Orders[i].BasketProd[z].Name);
+            }
+        }
+    }
 }
